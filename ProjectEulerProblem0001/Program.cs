9476d5b0ede939c33521a920152c9127f9a84b32
cadetce <cadetce@LCBLAB3120-36>
2024-10-24 09:57:33 -0400

    Addded documentation for the method

--- ProjectEulerProblem0001/Program.cs
+++ ProjectEulerProblem0001/Program.cs
@@ -14,6 +14,10 @@ namespace ProjectEulerProblem0001
             // Invoke th function
             Console.WriteLine("Project Euler Problem 001 " + SolveProjectEulerProblem001());
         }
+        /// <summary>
+        /// Find the sum of all the multiples of 3 or 5 or below 1000
+        /// </summary>
+        /// <returns>The solution to the problem</returns>
         static int SolveProjectEulerProblem001()
         {
             // Find the sum of all the multiples of 3 or 5 or below 1000
